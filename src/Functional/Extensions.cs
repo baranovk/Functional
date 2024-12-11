d9eceeceea392fe09540991105c3bcfb6e6669c5
baranovk <baranoffk@gmail.com>
2024-12-11 23:44:55 +0300

    Первый коммит

---
+++ src/Functional/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Functional;
+
+public static class Extensions
+{
+    public static T IterateUntil<T>(
+        this T @this,
+        Func<T, T> update,
+        Func<T, bool> endCondition)
+    {
+        var currentThis = @this;
+
+        while (!endCondition(currentThis))
+        {
+            currentThis = update(currentThis);
+        }
+
+        return currentThis;
+    }
+}
